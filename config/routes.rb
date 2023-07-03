01ec0982efff69523dc9415815079633e9735694
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-07-02 19:03:23 -0600

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,15 @@ Rails.application.routes.draw do
   resources :register_classes
   resources :courses
   resources :students
+
+  # Students
+  post '/signup', to: 'students#create'
+  get '/user', to: 'students#user'
+
+  # Sessions
+  post '/login', to 'sessions#create'
+  delete '/logout', to 'sessions#destroy'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
