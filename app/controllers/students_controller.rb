92a45bc0b792c6cb9208588d66443a993fa199a6
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-07-02 19:01:32 -0600

    Updated application controller, student controller, and session controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,37 @@
 class StudentsController < ApplicationController
+    before_action :find_student, only: [:show]
+    skip_before_action :authorize, only: [:create]
+
+    def index
+        render json: Student.all
+    end
+
+    def show
+        render json: @student
+    end
+
+    # POST /signup
+    def create
+        student = Student.new(student_params)
+        if student.save
+            session[:student_id] = student.id
+            render json: student, status: 201
+        else
+            render json: { errors: student.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    # GET /users
+    def user
+        render json: current_user
+    end
+
+    private
+    def find_student
+        @student = Student.find(params[:id])
+    end
+
+    def student_params
+        params.permit(:first_name, :last_name, :email, :password)
+    end
 end
