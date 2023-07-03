81ad4d73e4f6bf462732341c7741ea461658ae21
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-07-02 18:47:40 -0600

    established associations between all models

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,4 @@
 class Course < ApplicationRecord
+    has_many :register_classes, dependent: :destroy
+    has_many :students, through: :register_classes
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,6 @@
 class Student < ApplicationRecord
     has_secure_password
+
+    has_many :register_classes
+    has_many :courses, through: :register_classes
 end
